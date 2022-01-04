20525f7de353a5ddc5c1104ad415de1d47583413
WardMore <rafaelmserranov@gmail.com>
2022-01-04 13:45:07 -0300

    agrega modal en quienes somos section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
+$(document).ready(function (){
+
+  $("a").click(function (){
+    var gato= this.hash
+    
+
+    $("html, body").animate(
+      {
+        scrollTop: $(gato).offset().top -70
+      },
+      800
+    )
+  })
+})
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
   
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
